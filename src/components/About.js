b82a4383e6e29cb32197647931b8605e42c5470b
Durgesh Upadhyay <udurgesh6@gmail.com>
2022-07-25 17:03:32 +0530

    axios starting

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const About = () => {
+  const [employees, setEmployees] = useState([]);
+  const getEmpData = () => {
+    axios
+      .get("http://localhost:8080/employees?age=24&name=Durgesh")
+      .then((response) => {
+        console.log(response);
+        setEmployees(response.data);
+      });
+  };
   return (
     <div>
       <h2>I am About Component</h2>
+      <button onClick={getEmpData}>Get Employee Data</button>
+      {employees.map((emp) => (
+        <div key={emp.testing_id}>
+          <p>Name - {emp.name}</p>
+          <p>Age - {emp.age}</p>
+        </div>
+      ))}
     </div>
   );
 };
