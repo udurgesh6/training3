af2d97dcca69592f4670822a6e6f23b6f80dfde5
Durgesh Upadhyay <udurgesh6@gmail.com>
2022-08-10 18:48:19 +0530

    axios in detail

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,89 +1,57 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Button, Dropdown, Card, Image } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-  const [num, setNum] = useState(1);
-  const navigate = useNavigate();
-  const navigateTo = () => {
-    if (num % 2 === 0) {
-      navigate("/blogs");
-    } else {
-      navigate("/about");
-    }
-  };
-  return (
-    <div
-      style={{
-        border: "1px solid red",
-        backgroundColor: "blue",
-        color: "white",
-        height: "90%",
-      }}
-    >
-      <Image
-        src="https://i.ytimg.com/vi/7eCTVypgFNo/hq720_live.jpg?sqp=CNTF6pYG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCzA49htD1x4zedcwN8fsdUl52a8Q"
-        roundedCircle={true}
-      />
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Dropdown Button
-        </Dropdown.Toggle>
+  const [employees, setEmployees] = useState([]);
+  const [name, setName] = useState("");
+  const [testing_id, setTesting_id] = useState(0);
+  const [age, setAge] = useState(19);
 
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-      <h2>This is my Home Component</h2>
-      <p>{num}</p>
-      <button onClick={() => setNum((num) => num + 1)}>Inc</button>
-      <Button onClick={navigateTo}>Go to</Button>
-      <div
-        style={{
-          border: "1px solid red",
-          display: "flex",
-          flexDirection: "row",
-          height: "100px",
-          alignItems: "center",
-          justifyContent: "space-around",
-        }}
-      >
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </div>
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/employees")
+      .then((response) => {
+        console.log(response.data);
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Something went wrong");
+      });
+  }, []);
+
+  const addPerson = () => {
+    axios
+      .post("http://localhost:8080/employees/add", {
+        testing_id: testing_id,
+        name: name,
+        age: age,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setEmployees((employees) => [
+          ...employees,
+          {
+            testing_id: testing_id,
+            name: name,
+            age: age,
+          },
+        ]);
+      });
+  };
+
+  return (
+    <div>
+      {employees.map((emp, empid) => (
+        <p key={empid}>{emp.name}</p>
+      ))}
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        value={testing_id}
+        onChange={(e) => setTesting_id(e.target.value)}
+      />
+      <input value={age} onChange={(e) => setAge(e.target.value)} />
+      <button onClick={addPerson}>Add this person</button>
     </div>
   );
 };
