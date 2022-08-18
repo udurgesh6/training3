13287acf3f3e8983c86f7537797cb09df32eb4f6
Durgesh Upadhyay <udurgesh6@gmail.com>
2022-08-18 18:29:59 +0530

    useRef

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,15 @@
-import React, { useState, useReducer } from "react";
-const initialState = { count: 0, customer_name: "Durgesh", age: 24 };
-//Function  ==> initialState, Action
-const reducer = (state, action) => {
-  if (action.type === "increment") {
-    return {
-      count: state.count + action.payload,
-      customer_name: state.customer_name,
-      age: state.age,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      count: state.count - action.payload,
-      customer_name: state.customer_name,
-      age: state.age,
-    };
-  }
-  if (action.type === "changename") {
-    return {
-      count: state.count,
-      customer_name: action.payload,
-      age: state.age,
-    };
-  }
-};
+import React, { useRef } from "react";
+
 const Home = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const inputRef = useRef();
+  const runFunc = () => {
+    inputRef.current.focus();
+  };
   return (
     <div>
       <p>This is Home Component</p>
-      <p>Count - {state.count}</p>
-      <p>Name - {state.customer_name}</p>
-      <button onClick={() => dispatch({ type: "increment", payload: 2 })}>
-        Increment
-      </button>
-      <button onClick={() => dispatch({ type: "decrement", payload: 3 })}>
-        Decrement
-      </button>
-      <input
-        value={state.customer_name}
-        type="text"
-        onChange={(e) =>
-          dispatch({ type: "changename", payload: e.target.value })
-        }
-      />
+      <input ref={inputRef} />
+      <button onClick={runFunc}>Focus</button>
     </div>
   );
 };
